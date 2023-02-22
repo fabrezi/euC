5b16c390cac0c2b7913a49e01bd9b2c23125a289
fabrezi <fkschon@gmail.com>
2023-02-22 16:10:03 -0500

    shitt

--- set01/eu1.java
+++ set01/eu1.java
@@ -1,1 +1,20 @@
-//first euler question
+public class eu1{
+
+    public int matrix(int n){
+        int sum = 0;
+        int array = n;
+
+        if(n > sum){
+            System.out.println("you got mail");
+        } else{
+            System.out.println("loser");
+        }
+
+        return sum;
+    }
+
+    public static void main (String[] args){
+        int n = 100;
+    }
+
+}
